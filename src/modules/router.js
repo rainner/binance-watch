ed725414696b6b1540c122eb04e85151be8d2038
Rainner Lins <rainnerlins@gmail.com>
2018-05-18 23:37:09 -0400

    Added router module

---
+++ src/modules/router.js
@@ -0,0 +1,65 @@
+/**
+ * Basic URL hash router
+ */
+export default class Router {
+
+  // constructor
+  constructor() {
+    this._routes = {};
+    this._init();
+  }
+
+  // get routes data
+  getRoutes() {
+    return this._routes;
+  }
+
+  // set a url hash route
+  setRoute( route ) {
+    route = this._path( route );
+    window.location.hash = route;
+  }
+
+  // add custom route and callback to list
+  on( route, callback ) {
+    route = this._path( route );
+    if ( !route || typeof callback !== 'function' ) return;
+    this._routes[ route ] = callback;
+  }
+
+  // trigegr saved handler for a path
+  trigger( path ) {
+    path = this._path( path );
+    if ( !path ) return;
+
+    Object.keys( this._routes ).forEach( route => {
+      let cb = this._routes[ route ];
+
+      // path matches route, call handler as is
+      if ( path === route ) return cb();
+
+      // use regexp to test route
+      let reg = new RegExp( '^'+ route +'$' );
+      let matches = reg.exec( path );
+
+      // found something, pass regexp matches as arguments to handler
+      if ( matches ) {
+        matches = Array.from( matches );
+        matches.shift();
+        cb.apply( cb, matches );
+      }
+    });
+  }
+
+  // clean path
+  _path( path ) {
+    return '/'+ String( path || '' ).replace( /^[\#\/]+|[\/]+$/g, '' ).trim();
+  }
+
+  // start watching for hash changes
+  _init() {
+    window.addEventListener( 'hashchange', e => {
+      this.trigger( window.location.hash );
+    });
+  }
+}
