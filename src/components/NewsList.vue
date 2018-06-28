082294cc3a28a67f38065f43a57470d9947801a8
Rainner Lins <rainnerlins@gmail.com>
2018-06-28 12:55:02 -0400

    Added sentiment analysis to news list when viewing token info.

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,12 +1,15 @@
 <template>
   <section>
 
-    <div v-if="!pairData.token" class="push-bottom">
-      List of aggregated news and upcoming crypto events ({{ newsList.length }})
+    <div v-if="sentimentData" class="push-bottom">
+      <span class="text-bright text-clip">Sentiment for {{ pairData.name }} is &nbsp;</span>
+      <span class="text-gain icon-like iconLeft" v-if="sentimentData.score > 0">Positive (+{{ sentimentData.score }})</span>
+      <span class="text-loss icon-dislike iconLeft" v-else-if="sentimentData.score < 0">Negative ({{ sentimentData.score }})</span>
+      <span class="text-info icon-info iconLeft" v-else>Neutral ({{ sentimentData.score }})</span>
     </div>
 
-    <div v-if="!newsList.length" class="icon-info iconLeft text-grey">
-      <span v-if="pairData.token">There are no news/events for {{ pairData.token }}.</span>
+    <div v-if="!newsList.length" class="icon-info iconLeft text-grey push-bottom">
+      <span v-if="pairData.token">There are no news/events data for {{ pairData.name }}.</span>
       <span v-else>There are no news/events loaded.</span>
     </div>
 
@@ -22,6 +25,7 @@
 </template>
 
 <script>
+import sentiment from '../modules/sentiment';
 import utils from '../modules/utils';
 
 // component
@@ -33,23 +37,51 @@ export default {
     pairData: { type: Object, default: () => { return {} } },
   },
 
+  // component data
+  data() {
+    return {
+      sentimentData: null,
+    }
+  },
+
   // computed methods
   computed: {
 
-    // filter events for thos token
+    // get filtered list and emit list length
     newsList() {
-      let list = this.newsData.list || [];
-      let news = this.newsData;
-      let pair = this.pairData;
-
-      // filter by token and name
-      if ( pair.token ) {
-        list = utils.search( list, 'title', pair.token +'|'+ pair.name );
-      }
-      // update count outside
+      let list = this.getNewsList();
       this.$emit( 'listCount', list.length );
       return list;
     },
+  },
+
+  // component methods
+  methods: {
+
+    // get news list for a token, or all items
+    getNewsList() {
+      let list = this.newsData.list || [];
+      let pair = this.pairData;
+      if ( pair.token ) list = utils.search( list, 'title', pair.token +'|'+ pair.name );
+      return list;
+    },
+
+    // analize news data for token
+    analizeNewsList() {
+      let data = null;
+      let list = this.getNewsList();
+
+      if ( list.length && this.pairData.token ) {
+        let text = list.reduce( ( a, n ) => a += ' '+ n.title, '' );
+        data = sentiment.analyze( text );
+      }
+      this.sentimentData = data;
+    },
+  },
+
+  // on mounted
+  mounted() {
+    this.analizeNewsList();
   },
 }
 </script>
