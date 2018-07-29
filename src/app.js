2b389d87623fb937162c98fc823cb407704b8104
Rainner Lins <rainnerlins@gmail.com>
2018-07-29 15:46:28 -0400

    Moved sentiment module ref to app/vue prototype.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import Notify from './modules/notify';
 import Tooltip from './modules/tooltip';
 import store from './modules/store';
 import history from './modules/history';
+import sentiment from './modules/sentiment';
 import bus from './modules/bus';
 import utils from './modules/utils';
 import App from './components/App.vue';
@@ -26,6 +27,7 @@ Object.defineProperties( Vue.prototype, {
   $notify: { get: function() { return _notify; } },
   $store: { get: function() { return store; } },
   $history: { get: function() { return history; } },
+  $sentiment: { get: function() { return sentiment; } },
   $bus: { get: function() { return bus; } },
 });
 
