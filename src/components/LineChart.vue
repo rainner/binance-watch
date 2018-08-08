6b186b18fc4e54f73d4255fde905556ee5a1089d
Rainner Lins <rainnerlins@gmail.com>
2018-08-07 23:53:28 -0400

    Improved line chart component.

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,7 +1,8 @@
 <template>
-  <section class="linechart-wrap">
-    <svg :viewBox="svgBox">
+  <section class="linechart-wrap" :style="'height: '+ ( height - 10 ) +'px;'">
+    <svg :viewBox="svgBox" xmlns="http://www.w3.org/2000/svg">
       <polyline class="polyline" fill="none" stroke="#f0f0f0" stroke-width="2" stroke-linecap="round" :points="chartPoints" />
+      <circle class="circle" :cx="cx" :cy="cy" r="3" fill="#f0f0f0" stroke="none" />
     </svg>
   </section>
 </template>
@@ -18,8 +19,20 @@ export default {
     values: { type: Array, default: [], required: true },
   },
 
+  // data
+  data() {
+    return {
+      cx: 0,
+      cy: 0,
+    }
+  },
+
   // computed methods
   computed: {
+
+    circX() {
+
+    },
 
     // svg view box size
     svgBox() {
@@ -29,8 +42,11 @@ export default {
     // build points for SVG polyline
     chartPoints() {
       let data = utils.points( this.width, this.height, this.values );
-      let out  = data.map( d => d.x +','+ d.y );
-      return out.join( ' ' );
+      let last = data.length ? data[ data.length - 1 ] : { x: 0, y: 0 };
+      let list = data.map( d => ( d.x - 8 ) +','+ d.y );
+      this.cx  = last.x - 3;
+      this.cy  = last.y;
+      return list.join( ' ' );
     },
   },
 }
@@ -40,5 +56,6 @@ export default {
 .linechart-wrap {
   display: block;
   position: relative;
+  overflow: hidden;
 }
 </style>
